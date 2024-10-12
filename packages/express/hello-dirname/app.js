28b8b0340e74b08009cc2ac079ff5b554e41d1bc
Takahito Urai <uraitakahito@gmail.com>
2024-10-12 07:42:23 +0000

    Add a sample using dirname

---
+++ packages/express/hello-dirname/app.js
@@ -0,0 +1,13 @@
+// https://flaviocopes.com/fix-dirname-not-defined-es-module-scope/
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+export var app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.get('/', (req, res) => {
+  res.send(__dirname);
+});
