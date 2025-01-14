3de3c69b82cdbce8c43fa923d473b60c7d09f040
Takahito Urai <uraitakahito@gmail.com>
2025-01-14 11:36:54 +0900

    Update README and refactor fizzbuzz package: improve script commands and change var to let

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ```sh
 npm ci
-npm run start -w packages/ps-tree
-npm run start -w packages/import-internal-esmodule
+npm run start -w ps-tree
+npm run start -w import-internal-esmodule
 npm run test -ws --if-present
 ```
--- packages/fizzbuzz/fizzbuzz.js
+++ packages/fizzbuzz/fizzbuzz.js
@@ -1,17 +1,17 @@
 /* eslint-disable no-console */
 function fizzbuzz(n) {
-    if (n % 15 === 0) {
-        return 'FizzBuzz';
-    }
-    if (n % 3 === 0) {
-        return 'Fizz';
-    }
-    if (n % 5 === 0) {
-        return 'Buzz';
-    }
-    return n;
+  if (n % 15 === 0) {
+    return 'FizzBuzz';
+  }
+  if (n % 3 === 0) {
+    return 'Fizz';
+  }
+  if (n % 5 === 0) {
+    return 'Buzz';
+  }
+  return n;
 }
-var a = 1;
+let a = 1;
 console.log(fizzbuzz(a += 1));
 console.log(fizzbuzz(a += 1));
 console.log(fizzbuzz(a += 1));
